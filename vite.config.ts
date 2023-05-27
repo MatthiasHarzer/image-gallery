49b63091a505fdec287359ad44eb474123eebb7a
Matthias Harzer <matthias.harzer03@gmail.com>
2023-05-27 02:29:20 +0200

    PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'script',
+      manifest: {
+        name: "Image Gallery App",
+        short_name: "Image Gallery App",
+        start_url: "/",
+        display: "standalone",
+        theme_color: "#646cff",
+        icons:[
+          {
+            "src": "pwa-icons/manifest-icon-192.maskable.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "maskable any"
+          },
+          {
+            "src": "pwa-icons/manifest-icon-512.maskable.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable any"
+          }
+        ]
+      }
+    })
+  ],
 })
